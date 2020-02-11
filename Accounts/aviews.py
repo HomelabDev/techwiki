aefec20debb914326639c07140fa91551d33a31b
HomelabDev <57008111+HomelabDev@users.noreply.github.com>
2020-02-11 18:53:44 +0100

    Add files via upload
    upload static

---
+++ Accounts/aviews.py
@@ -0,0 +1,90 @@
+###############
+###Imports#####
+###############
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .models import Article
+import datetime
+
+
+
+#############
+##register###
+#############
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/login/')
+    else:
+        form = UserCreationForm()
+
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            'form':form
+        }
+        
+   )
+#############
+##dashboard##
+#############
+
+def dashboard(request):
+    username = request.user
+
+
+    obj = Article.objects.filter(user=username)#article
+
+    context = {
+            'object':  obj
+        }
+
+    return render(
+        request,
+        "dashboard/index.html", context)
+
+def post(request):
+    ############
+    #save#to#db#
+    ############
+    if request.method == 'POST':
+        title = request.POST['title']
+        picture = request.POST['picture']
+        mainContent = request.POST['mainContent']
+        user = request.POST['username']
+        date = datetime.datetime.now()
+        url = request.POST['url']
+
+
+        article = Article(user = user, title=title, picture = picture, mainContent = mainContent, date = date, url = url)
+        article.save()
+
+        return redirect('dashboard')
+
+
+
+    return render(
+        request,
+        "dashboard/post.html",
+        {
+            
+        }
+   )
+
+def delete_account(request):
+    username = request.user
+
+    if request.method == 'POST':
+        username.delete()
+        return redirect('/')
+
+    return render(
+        request,
+        "dashboard/account_delete.html")
+
---
+++ WebApp/urls.py
@@ -0,0 +1,36 @@
+"""
+Definition of urls for WebApp.
+"""
+
+from datetime import datetime
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from Main import mviews
+from Accounts import aviews
+
+urlpatterns = [
+    #############################
+    ####Login & Registration#####
+    #############################
+    path('', include('django.contrib.auth.urls')),
+    path('register/', aviews.register, name="register"),
+    #############################
+    ###########Articles##########
+    #############################
+    path('dashboard/', aviews.dashboard, name='dashboard'),
+    path('dashboard/post/', aviews.post, name='post'),
+    path('dashboard/delete', aviews.delete_account, name="delete_account"),
+    #############################
+    ##########Articles###########
+    #############################
+    path('article/<slug:slug>/', mviews.article_dynamic, name='article_dynamic'),
+    path('article/<slug:slug>/delete', mviews.article_delete, name='article_delete'),
+    path('article/<slug:slug>/edit', mviews.article_edit, name='article_edit'),
+    path('article/', mviews.article, name='article'),
+    #############################
+    ########Main Content#########
+    #############################
+    path('', mviews.home, name="home"),
+    path('admin/', admin.site.urls),
+]
